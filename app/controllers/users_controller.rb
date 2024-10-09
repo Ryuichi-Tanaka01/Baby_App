25ff8c0610157c07d4b2f8417022f8c0875688f5
Ryuichi Tanaka <ryu@RyuichinoMac-mini.local>
2024-10-10 06:24:55 +0900

    createpage実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     puts params.inspect
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to "/users/page"
     else
       puts @user.errors.full_messages
