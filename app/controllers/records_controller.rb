e7ea12213b1fe69a3957012a2448792f1c2e6fe1
Ryuichi Tanaka <ryu@RyuichinoMac-mini.local>
2024-10-12 14:40:56 +0900

    データ表示画面の修正

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -4,20 +4,22 @@ class RecordsController < ApplicationController
     end
   
     def create
-      Rails.logger.debug(params.inspect) 
+      Rails.logger.debug(params.inspect)
       @record = Record.new(record_params)
+    
       if @record.save
-        redirect_to users_path(current_user)
+        redirect_to page_users_path, notice: '記録が保存されました。'
       else
-        redirect_to "/users/page"
+        flash.now[:alert] = @record.errors.full_messages.to_sentence
+        render :new
       end
     end
+    
   
     private
   
     def record_params
-      params.require(:record).permit(:recorder_id, :flight_category_id).merge(user_id: params[:user_id])
+      params.require(:record).permit(:recorder_id, :flight_category_id).merge(user_id: current_user.id)
     end
   end
-  
   
